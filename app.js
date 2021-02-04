6920c210ba109666c59a4af6db301b7cbc128f17
Vivek Dubey <dubey.vivek.2805@gmail.com>
2021-02-04 15:21:10 +0530

    added the GET API for TODO List

--- app.js
+++ app.js
@@ -4,6 +4,25 @@ const http = require('http');
 
 const port = 8081;
 
+// todoList array
+
+let todoList = ["Complete Node Byte", "Play Cricket"];
+
+function getTodoList() {
+    let htmlTodoList = '';
+
+    todoList.forEach((value) => {
+        htmlTodoList += '<li>' + value + '</li>';
+    });
+
+    // create an ordered list
+
+    htmlTodoList = '<ol>' + htmlTodoList + '</ol>';
+
+    // return the HTML code
+
+    return '<h1>TODO</h1>' + htmlTodoList + '<p>Created by : Vivek Dubey</p>';
+}
 
 http.createServer((request, response) => {
 
@@ -19,17 +38,19 @@ http.createServer((request, response) => {
 
         // Set response body i.e, data to be sent
 
-        response.write('<h1>TODO</h1> <p>Created by : Vivek Dubey</p>');
+        response.write(getTodoList());
+
     } else if(method != 'GET') {
 
         // when the method is not GET, url can be anything
 
         response.writeHead(501, { 'Status-Code' : 'Not-Implemented' });
+
     } else {
 
         // when url is wrong
         // what if method is 'GET' - same
-        // what if method is not 'GET' - will execute not implemented
+        // what if method is not 'GET' - will execute "not implemented"
         
         response.writeHead(404, { 'Status-Code' : 'Not-Found' });
     }
